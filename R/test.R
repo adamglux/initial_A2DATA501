09a9a5161b94f6cdb086ccc7b15ba458f5d205cb
Adam Glux <118312722+adamglux@users.noreply.github.com>
2024-08-14 13:46:56 +1200

    update to test files with new function name

--- R/test.R
+++ R/test.R
@@ -1,35 +1,35 @@
-context("Testing influencer context")
+context("Testing influenceR context")
 
 ## invalid inputs 
-test_that("function influencer gives helpful errors", 
+test_that("function influenceR gives helpful errors", 
           {
-            expect_error(influencer(model1, matrix(data1)), 
+            expect_error(influenceR(model1, matrix(data1)), 
                          "Input data must be a data.frame.")
             
-            expect_error(influencer(mtcars, mtcars), 
+            expect_error(influenceR(mtcars, mtcars), 
                          "Input model must be an lm or glm object.")
             
-            expect_error(influencer(model6, data6),
+            expect_error(influenceR(model6, data6),
                          "Input data must have at least 2 columns.")
             
-            expect_error(influencer(model5, data5), 
+            expect_error(influenceR(model5, data5), 
                          "Dataset is too small")
             
-            expect_error(influencer(model7, data7), 
+            expect_error(influenceR(model7, data7), 
                          "Dataset is the wrong dimension")
             
-            expect_error(influencer(model4, data4), 
+            expect_error(influenceR(model4, data4), 
                          "Input data contains infinite values.")
             
-            expect_error(influencer(model3, data3), 
+            expect_error(influenceR(model3, data3), 
                          "Input data contains more than 5% NA values.")
             
-            expect_error(influencer(model1, data1, plot = "phil collins"), 
+            expect_error(influenceR(model1, data1, plot = "phil collins"), 
                          "Input for plot must be either: none, all, cooks, DFFITS, or Hadi")
             
-            output <- influencer(model1, data1, plot = "none")
+            output <- influenceR(model1, data1, plot = "none")
             expect_true(is.list(output))
-            expect_equal(names(output$influencers), c("Cook's D", "DFFITs", "Hadi's Influence"))
+            expect_equal(names(output$influenceRs), c("Cook's D", "DFFITs", "Hadi's Influence"))
             
             
             # Assuming pre-calculated values for a specific row for testing
@@ -40,15 +40,15 @@ test_that("function influencer gives helpful errors",
           })
 
 ## valid inputs
-test_that("function influencer works with dataframes, lm/glm objects, and plots", {
+test_that("function influenceR works with dataframes, lm/glm objects, and plots", {
   
   pdf(NULL)
   on.exit(dev.off())
   
-  expect_silent(influencer(model1, mtcars, plot = "none"))
-  expect_silent(influencer(model1, mtcars, plot = "cooks"))
-  expect_silent(influencer(model1, mtcars, plot = "DFFITS"))
-  expect_silent(influencer(model1, mtcars, plot = "Hadi"))
-  expect_silent(influencer(model1, mtcars, plot = "all"))
+  expect_silent(influenceR(model1, mtcars, plot = "none"))
+  expect_silent(influenceR(model1, mtcars, plot = "cooks"))
+  expect_silent(influenceR(model1, mtcars, plot = "DFFITS"))
+  expect_silent(influenceR(model1, mtcars, plot = "Hadi"))
+  expect_silent(influenceR(model1, mtcars, plot = "all"))
   
 })
